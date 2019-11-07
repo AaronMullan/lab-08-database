2aaa48c6b88a923df0696026f6cb7b112785140e
Nikki <nikki.satopdx@gmail.com>
2019-11-07 10:27:08 -0800

    merged conflicts

--- data/cities.js
+++ data/cities.js
@@ -4,53 +4,31 @@ module.exports = [
         year: 1851,
         isWestCoast: true,
         nickname: 'City of Roses'
-<<<<<<< HEAD
-    }, 
-    {   
-=======
     },
     {
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
         name: 'New York City, New York',
         year: 1624,
         isWestCoast: false,
         nickname: 'The Big Apple'
-<<<<<<< HEAD
-    }, 
-=======
     },
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
     {
         name: 'Seattle, Washington',
         year: 1851,
         isWestCoast: true,
         nickname: 'Emerald City'
-<<<<<<< HEAD
-    }, 
-
-=======
     },
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
     {
         name:'Baltimore, Maryland',
         year: 1729,
         isWestCoast: false,
         nickname: 'Charm City'
-<<<<<<< HEAD
-    }, 
-=======
     },
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
     {
         name: 'New Orleans, Louisiana',
         year: 1718,
         isWestCoast: false,
         nickname: 'The Big Easy'
-<<<<<<< HEAD
-    }, 
-=======
     },
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
     {
         name: 'Dallas, Texas',
         year: 1841,
@@ -63,24 +41,10 @@ module.exports = [
         isWestCoast: true,
         nickname: 'El Lay'
     },
-<<<<<<< HEAD
-
-=======
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
     {
         name: 'Orlando, Florida',
         year: 1885,
         isWestCoast: false,
         nickname:'The City Beautiful'
     },
-<<<<<<< HEAD
-
 ];
-
-
-
-
-
-=======
-];
->>>>>>> f36b7d28a071cf0dd3d92d8d5bbc6f060fb6abad
