6fc7957ae8e64656a6da54b56885e1beea6a2fe1
Nikki <nikki.satopdx@gmail.com>
2019-11-07 10:13:41 -0800

    initial commit

---
+++ data/cities.js
@@ -0,0 +1,58 @@
+module.exports = [
+    {
+        name: 'Portland, Oregon',
+        year: 1851,
+        isWestCoast: true,
+        nickname: 'City of Roses'
+    }, 
+    {   
+        name: 'New York City, New York ',
+        year: 1624,
+        isWestCoast: false,
+        nickname: 'The Big Apple'
+    }, 
+    {
+        name: 'Seattle, Washington',
+        year: 1851,
+        isWestCoast: true,
+        nickname: 'Emerald City'
+    }, 
+
+    {
+        name:'Baltimore, Maryland',
+        year: 1729,
+        isWestCoast: false,
+        nickname: 'Charm City'
+    }, 
+    {
+        name: 'New Orleans, Louisiana',
+        year: 1718,
+        isWestCoast: false,
+        nickname: 'The Big Easy'
+    }, 
+    {
+        name: 'Dallas, Texas',
+        year: 1841,
+        isWestCoast: false,
+        nickname: 'FunkyTown'
+    },
+    {
+        name: 'Los Angeles, California',
+        year: 1781,
+        isWestCoast: true,
+        nickname: 'El Lay'
+    },
+
+    {
+        name: 'Orlando, Florida',
+        year: 1885,
+        isWestCoast: false,
+        nickname:'The City Beautiful'
+    },
+
+];
+
+
+
+
+
