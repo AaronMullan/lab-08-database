e4167e52f60804ff85a56dc580d4a0b385c8159a
Nikki <nikki.satopdx@gmail.com>
2019-11-08 08:59:16 -0800

    git push -u origin nikki-branch

---
+++ public/cities-list/citiesApp.js
@@ -0,0 +1,31 @@
+import Component from '../Component.js';
+import Header from '../common/Header.js';
+import CitiesList from './citiesList.js';
+import { getCities } from '../services/domain-api.js';
+
+class CityListApp extends Component {
+
+    onRender(dom) {
+        const header = new Header({ title: 'List of City' });
+        dom.prepend(header.renderDOM());
+
+        const list = new CitiesList({ city: [] });
+        const main = dom.querySelector('main');
+        main.appendChild(list.renderDOM());
+
+        getCities().then(city => {
+            list.update({ city });
+        });
+    }
+
+    renderHTML() {
+        return /*html*/`
+            <div>
+                <!-- header goes here -->
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default CityListApp;
