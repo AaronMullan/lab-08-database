91ecde0f77ccec158d15cb4d6d41dc7a6817df85
Nikki <nikki.satopdx@gmail.com>
2019-11-08 08:40:38 -0800

    heroku add on

--- data/cities.js
+++ data/cities.js
@@ -3,48 +3,57 @@ module.exports = [
         name: 'Portland, Oregon',
         year: 1851,
         isWestcoast: true,
-        nickname: 'City of Roses'
+        nickname: 'City of Roses',
+        region: 'Pacific Northwest'
     },
     {
         name: 'New York City, New York',
         year: 1624,
         isWestcoast: false,
-        nickname: 'The Big Apple'
+        nickname: 'The Big Apple',
+        region: 'North-east'
     },
     {
         name: 'Seattle, Washington',
         year: 1851,
         isWestcoast: true,
-        nickname: 'Emerald City'
+        nickname: 'Emerald City',
+        region: 'Pacific Northwest'
     },
     {
         name:'Baltimore, Maryland',
         year: 1729,
         isWestcoast: false,
-        nickname: 'Charm City'
+        nickname: 'Charm City',
+        region: 'North-east'
     },
     {
         name: 'New Orleans, Louisiana',
         year: 1718,
         isWestcoast: false,
-        nickname: 'The Big Easy'
+        nickname: 'The Big Easy',
+        region: 'South-east'
     },
     {
         name: 'Dallas, Texas',
         year: 1841,
         isWestcoast: false,
-        nickname: 'FunkyTown'
+        nickname: 'FunkyTown',
+        region: 'Southern Plains'
     },
     {
         name: 'Los Angeles, California',
         year: 1781,
         isWestcoast: true,
-        nickname: 'El Lay'
+        nickname: 'El Lay',
+        region: 'Southwest'
+
     },
     {
         name: 'Orlando, Florida',
         year: 1885,
         isWestcoast: false,
-        nickname:'The City Beautiful'
+        nickname:'The City Beautiful',
+        region: 'Southeast'
     },
 ];
