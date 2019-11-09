f1f97b818335500ae4d2cc7b5c2f33e4908788b2
Nikki <nikki.satopdx@gmail.com>
2019-11-08 16:07:32 -0800

    css stuff

--- public/common/Footer.js
+++ public/common/Footer.js
@@ -5,14 +5,9 @@ class Header extends Component {
 
         return /*html*/` 
         <div class="header">
-
         <img class="icon" src="./asset/cityscape.png">
             <h1 > Know your cities?</h1>
-             <img class="icon" src="./asset/cityscape.png">
-        <div class="buttons">
-             <a class ="button" href ="./cities-list.html">Cities</a>
-             <a class ="button" href ="./cities-list.html">Add City</a>
-            </div>
+            <img class="icon" src="./asset/cityscape.png">
         </div>
         `;
         
